7078a8c3a69d9510211e06da8a3457b247cf710d
paria khaledi <pariakhaledi0@gmail.com>
2025-02-18 00:42:15 +0330

    main page

--- src/main.js
+++ src/main.js
@@ -1,19 +1,20 @@
 
- let menubar = document.querySelector(".menubar");
- let submenu = document.querySelector(".submenu");
+let menubar = document.querySelector(".menubar");
+let submenu = document.querySelector(".submenu");
 
- menubar.addEventListener("click", function () {
-   if (submenu.classList.contains("hidden")) {
-     submenu.classList.remove("hidden"); 
-     setTimeout(function () {
-       submenu.classList.remove("scale-y-0"); 
-       submenu.classList.add("scale-y-100");
-     }, 10);
-   } else {
-     submenu.classList.remove("scale-y-100"); 
-     submenu.classList.add("scale-y-0"); 
-     setTimeout(function () {
-       submenu.classList.add("hidden"); 
-     }, 300);
-   }
- });
+menubar.addEventListener("click", function () {
+  if (submenu.classList.contains("hidden")) {
+    submenu.classList.remove("hidden");
+    setTimeout(function () {
+      submenu.classList.remove("scale-y-0");
+      submenu.classList.add("scale-y-100");
+    }, 10);
+  } else {
+    submenu.classList.remove("scale-y-100");
+    submenu.classList.add("scale-y-0");
+    setTimeout(function () {
+      submenu.classList.add("hidden");
+    }, 300);
+  }
+});
+
